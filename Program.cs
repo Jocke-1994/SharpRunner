f509054180a86717c4d937072991e43fb0c7d413
Joakim Rehn <jocke.94@hotmail.se>
2026-01-20 15:54:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LabyrinthGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
